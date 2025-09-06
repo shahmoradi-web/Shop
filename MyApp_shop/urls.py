7c0d3f6fe90b49b7d57f6a7c2ee976b078f667d3
narges <narges.shahmoradi.01@gmail.com>
2025-09-06 22:25:36 +0330

    cart feature

--- MyApp_shop/urls.py
+++ MyApp_shop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static,settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls'), name='shop'),
+    path('', include('cart.urls'), name='cart'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ cart/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .models import *
+
+app_name='cart'
+urlpatterns = [
+
+]
