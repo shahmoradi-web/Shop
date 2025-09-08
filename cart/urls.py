ff522d54638d8ca381f0b3abef2132137489e220
narges <narges.shahmoradi.01@gmail.com>
2025-09-08 22:48:08 +0330

    add cart feature

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import views
 
 from .models import *
 
 app_name='cart'
 urlpatterns = [
+    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
 
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+
+from django.http import JsonResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from shop.models import Product
+from .cart import Cart
 
 # Create your views here.
+
+@require_POST
+@csrf_exempt
+def add_to_cart(request, product_id):
+    try:
+        cart = Cart(request)
+        product = get_object_or_404(Product, pk=product_id)
+        cart.add(product)
+        context = {
+            'item_count':len(cart),
+            'total_price':cart.get_total_price()
+        }
+        # send_sms_normal('09911660921', 'send SMS')
+        return JsonResponse(context)
+    except:
+        return JsonResponse({'error':'Something went wrong'})
